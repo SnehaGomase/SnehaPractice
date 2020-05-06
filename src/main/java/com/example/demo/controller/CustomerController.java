1533f0866362a81235b742c8fa285194be2350de
SnehaGomase <snehagomasee@gmail.com>
2020-05-06 12:52:36 +0530

    First commit of the day

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController
+{
+public String saveCustomerDetails()
+{
+return "Customer Controller Modified";
+}
+}
---
+++ src/main/java/com/example/demo/controller/LOginController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LOginController
+{
+public String login()
+{
+return "in Login Controller";
+}
+
+}
